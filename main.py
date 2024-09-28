3c6876e2e5a2a773be2da779444149e74e1d9cf2
AgrimSomani <asomani@connect.hku.hk>
2024-09-28 11:13:26 +0800

    changes

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
-from typing import Union
-
+from typing import List
+from collections import defaultdict
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class MoveRequest(BaseModel):
+    board: str
+    moves: str
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+@app.post("/klotski")
+async def klotski(move_requests: List[MoveRequest]):
+    res_total = []
+    def move(direction, r,c):
+        return (r+1,c) if direction == 'S' else (r-1,c) if direction == 'N' else (r,c+1) if direction == 'E' else (r,c-1)
+
+    for board_a, moves_a in move_requests:
+        blocks = defaultdict(list)
+        block_move = {}
+        board,moves = board_a[1], moves_a[1]
+
+        for r in range(5):
+            for c in range(4):
+                cur = board[(r*4) + c]
+                blocks[cur].append((r,c))
+                if cur not in block_move:
+                    block_move[cur] = (0,0)
+        
+        l = 0
+
+        while l != len(moves):
+            block = moves[l]
+            d = moves[l+1]
+            cur_r,cur_c = block_move[block]
+            block_move[block] = move(d,cur_r,cur_c)
+            l+=2
+            
+        res = [['@' for c in range(4)] for r in range(5)]
+        
+        for block in blocks:
+            if block == "@":
+                continue
+            for r,c in blocks[block]:
+                new_r,new_c = block_move[block]
+                res[r+new_r][c+new_c] = block
+                
+        for r in range(len(res)):
+            res[r] = "".join(res[r])
+        
+        res_total.append("".join(res))
+
+    return res_total    
